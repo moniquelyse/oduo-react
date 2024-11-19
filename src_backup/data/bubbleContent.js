61fcaac47e29e6a4f0621152c30c440e7b7647b5
Andrea B <moniquelyse@users.noreply.github.com>
2024-11-19 00:04:48 -0300

    React app part 15

--- src_backup/data/bubbleContent.js
+++
@@ -1,8 +0,0 @@
-export const bubbleContent = {
-  stage1: {
-    title: "La iniciación",
-    text: "Tu educación financiera, tus experiencias, sentimientos y preferencias forman tu Personalidad Financiera®. Descúbrela en el test a continuación.",
-    buttonText: "Empezar"
-  },
-  // ... resto del contenido que ya tienes
-};

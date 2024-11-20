8f0b9b5ef4dc765b49c03a78d0c5ecf6fd428f36
Andrea B <moniquelyse@users.noreply.github.com>
2024-11-20 18:17:51 -0300

    Added console mesage for intruders

---
+++ src/utils/devTools.js
@@ -0,0 +1,15 @@
+const devToolsMessage = () => {
+  const style = [
+    'font-size: 12px',
+    'color: #ffffff',
+    'background-color: #000000',
+    'padding: 10px',
+    'border-radius: 5px',
+    'border: 1px solid #ffffff'
+  ].join(';');
+
+  console.log('%cHola 👋 ¿todo bien? Escríbeme a andrea@ordenar.me', style);
+
+};
+
+export default devToolsMessage; 
